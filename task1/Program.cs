f1d0b6db9b54ce7297c54e1523ecf07bc9f3b04d
Dima <dimjke-72@mail.ru>
2023-02-03 22:31:52 +0500

    tasks1-2 completed

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine() ?? "0");
+if (num2 * num2 == num1)
+     Console.WriteLine("Число " + num1 + " является квадратом числа " + num2);
+else
+    Console.WriteLine("Число {0} не является квадратом числа {1}", num1, num2);
--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите день недели от 1 до 7: ");
+int day = int.Parse(Console.ReadLine() ?? "0");
+switch (day)
+{
+    case 1: Console.WriteLine("Понедельник"); break;
+    case 2: Console.WriteLine("Вторник"); break;
+    case 3: Console.WriteLine("Среда"); break;
+    case 4: Console.WriteLine("Четверг"); break;
+    case 5: Console.WriteLine("Пятница"); break;
+    case 6: Console.WriteLine("Суббота"); break;
+    case 7: Console.WriteLine("Воскресенье"); break;
+    default: Console.WriteLine("Вы ввели некорректное число"); break;
+}
