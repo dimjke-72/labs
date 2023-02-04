9291d40fcc6028e04f3e69b07c101862f81dc4a8
Dima <dimjke-72@mail.ru>
2023-02-04 09:59:41 +0500

    task5 added

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int result = 0;
+if (number>99 && number<1000)
+{
+    result = number % 10;
+    Console.WriteLine("Последняя цифра числа {0} - {1}", number, result);
+}
+else
+    Console.WriteLine("Вы ввели не трехзначное число");
