ae980fd015ee47c3babc3606c1785a3e73d318b7
Dima <dimjke-72@mail.ru>
2023-02-04 09:50:25 +0500

    tasks3-4 added

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if (number % 2 == 0)
+    Console.WriteLine("Число {0} - четное", number);
+else
+    Console.WriteLine("Число {0} - нечетное", number);
--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0)
+        Console.WriteLine("Число {0} - четное", i);
+    else
+    //continue; - так можно делать вместо следубщей строки и есть ли смысл??
+    Console.WriteLine("Число {0} - нечетное", i);
+}
